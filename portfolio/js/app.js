a873378b054317d7b051da06a7ee1ad240b43448
stefannivinat <95473284+stefannivinat@users.noreply.github.com>
2022-03-23 11:33:16 +0100

    Add files via upload

---
+++ portfolio/js/app.js
@@ -0,0 +1,7 @@
+function cambiarclase(){
+    let siteNav = document.getElementById('site-nav')
+        siteNav.classList.toggle('site-nav-open')
+
+    let menuOpen= document.getElementById('menu-toggle');
+        menuOpen.classList.toggle('menu-open');
+}
